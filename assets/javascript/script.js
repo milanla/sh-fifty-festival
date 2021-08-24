5628a397a8737dd8d5a719e61257ff70de0be749
Milan <milan.lanlada@gmail.com>
2021-08-23 23:10:27 -0400

    add js animation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,9 +8,14 @@ function makeMarquee() {
 
 makeMarquee()
 
-const circles = document.querySelectorAll('.circle')
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max + min + 1)) + min
+}
 
-circles.forEach(function(circle, index) {
+const circles = document.querySelectorAll('.circle')
+const squiggles = document.querySelectorAll('.squiggle')
+
+circles.forEach((circle, index) => {
   circle.animate([
     { transform: 'scale(1)' },
     { transform: 'scale(1.3)' },
@@ -18,6 +23,20 @@ circles.forEach(function(circle, index) {
   ], {
     delay: 300 * index,
     duration: 3000,
+    iterations: Infinity
+  })
+})
+
+squiggles.forEach((squiggle, index) => {
+  const randomNum = random(0, 45)
+
+  squiggle.animate([
+    { transform: 'rotate(0deg)' },
+    { transform: `rotate(${randomNum}deg)` },
+    { transform: 'rotate(0deg)' }
+  ], {
+    delay: 300 * index,
+    duration: 5000,
     iterations: Infinity
   })
 })
